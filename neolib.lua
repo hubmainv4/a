5281ea45f0c752d6d6178c406a1500a25a074776
hubmainv4 <ezcrypxdd@gmail.com>
2025-08-11 16:32:00 +0300

    Update neolib.lua

--- neolib.lua
+++ neolib.lua
@@ -819,7 +819,6 @@ PresetThemes:Dropdown({
 	Name = "Presets",
 	Flag = "UI/Presets",
 	Options = {
-        "CryptHub Official"
 		"Tokyo Night",
 		"Kanagawa",						
 		"Quartz",
@@ -879,15 +878,6 @@ PresetThemes:Dropdown({
 	State = "Quartz",
 	Callback = function(v)
 		local themes = {
-
-
-					["CryptHub Official"] = {
-						FontColor = "#ffffff",
-						MainColor = "#2e2e2e",
-						Accent = "#6a00ff",
-						BackgroundColor = "#1c1c1c",
-						OutlineColor = "#000000"
-					},
 					['Tokyo Night'] = {
 			                        FontColor = "#FFFFFF",
 			                        MainColor = "#191925",
